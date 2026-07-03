9b7c234663472edca5a5153bf0b7fb1e46f4d747
Idrissa Kamara <graceandtruth30@gmail.com>
2026-07-03 15:17:59 -0700

    Save pending patient controller changes before push

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -6,6 +6,7 @@ exports.createPatient = async (req, res) => {
     const patient = await Patient.create(req.body);
     res.status(201).json(patient);
   } catch (err) {
+    console.log(err);
     res.status(500).json({ error: err.message });
   }
 };
